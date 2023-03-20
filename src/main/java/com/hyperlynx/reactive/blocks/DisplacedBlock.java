6b88c1b53219b5443bfd2210f3103588e3a6321e
Hyperlynx <hjake123@gmail.com>
2023-03-19 23:16:09 -0500

    Breaking a displaced block places the block it contains

--- src/main/java/com/hyperlynx/reactive/blocks/DisplacedBlock.java
+++ src/main/java/com/hyperlynx/reactive/blocks/DisplacedBlock.java
@@ -6,6 +6,8 @@ import com.hyperlynx.reactive.util.HarvestChecker;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.BlockGetter;
@@ -13,6 +15,7 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.PushReaction;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
@@ -25,7 +28,9 @@ public class DisplacedBlock extends Block implements EntityBlock {
                 .isViewBlocking((BlockState state, BlockGetter getter, BlockPos pos) -> false)
                 .noOcclusion()
                 .noLootTable()
-                .strength(45F));
+                .strength(1F)
+                .explosionResistance(-1F)
+                .sound(SoundType.CHAIN));
     }
 
     @Override
@@ -41,7 +46,6 @@ public class DisplacedBlock extends Block implements EntityBlock {
     // Convert some other block into a Displaced Block.
     public static void displace(BlockState state_to_be_displaced, BlockPos pos, Level level, int duration){
         displaceWithChain(state_to_be_displaced, pos, level, duration, null);
-        // Chain target will be null, indicating no chain.
     }
 
     // Convert some other block into a Displaced Block, and then link it to the chain target.
@@ -52,13 +56,12 @@ public class DisplacedBlock extends Block implements EntityBlock {
             return;
         }
 
-        level.setBlock(pos, Registration.DISPLACED_BLOCK.get().defaultBlockState(), Block.UPDATE_CLIENTS);
+        level.setBlockAndUpdate(pos, Registration.DISPLACED_BLOCK.get().defaultBlockState());
         BlockEntity be = level.getBlockEntity(pos);
-        if(!(be instanceof DisplacedBlockEntity)){
+        if(!(be instanceof DisplacedBlockEntity displaced)){
             System.err.println("Displaced Block Entity didn't attach...? Report this to hyperlynx!");
             return;
         }
-        DisplacedBlockEntity displaced = (DisplacedBlockEntity)be;
 
         displaced.self_state = state_to_be_displaced;
         displaced.chain_target = chain;
@@ -88,7 +91,17 @@ public class DisplacedBlock extends Block implements EntityBlock {
             return;
         }
 
-        level.setBlock(pos, ((DisplacedBlockEntity) blockentity).self_state, Block.UPDATE_CLIENTS);
+        level.setBlockAndUpdate(pos, ((DisplacedBlockEntity) blockentity).self_state);
+    }
+
+    // When broken, the displacement should end.
+    @Override
+    public boolean onDestroyedByPlayer(BlockState state, Level level, BlockPos pos, Player player, boolean willHarvest, FluidState fluid) {
+        if(level.getBlockEntity(pos) instanceof DisplacedBlockEntity displaced){
+            level.setBlockAndUpdate(pos, displaced.self_state);
+        }
+        level.playSound(null, pos, SoundEvents.CHAIN_BREAK, SoundSource.PLAYERS, 1.0F, 0.8F);
+        return true;
     }
 
     @Nullable
@@ -104,13 +117,6 @@ public class DisplacedBlock extends Block implements EntityBlock {
     @Override
     public VoxelShape getCollisionShape(BlockState state, BlockGetter getter, BlockPos pos, CollisionContext context) {
         return Shapes.empty();
-    }
-
-    @Override
-    protected void spawnDestroyParticles(Level level, Player p_152423_, BlockPos pos, BlockState p_152425_) {
-        for(int i = 0; i < 7; i++){
-            level.addParticle(ParticleTypes.ELECTRIC_SPARK, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, 0, 0, 0);
-        }
     }
 
     @Override
