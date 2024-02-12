039b936c901c1e2c45a877ee82d7e1b08e077107
hjake123 <hjake123@gmail.com>
2024-02-11 23:13:58 -0600

    Save DataGenerationMan from devastation !

--- src/main/java/com/hyperlynx/reactive/ReactiveMod.java
+++ src/main/java/com/hyperlynx/reactive/ReactiveMod.java
@@ -2,6 +2,7 @@ package com.hyperlynx.reactive;
 
 import com.hyperlynx.reactive.alchemy.SpecialCaseMan;
 import com.hyperlynx.reactive.alchemy.rxn.ReactionMan;
+import com.hyperlynx.reactive.datagen.DataGenerationMan;
 import com.hyperlynx.reactive.integration.create.ReactiveCreatePlugin;
 import com.hyperlynx.reactive.util.ConfigMan;
 import com.hyperlynx.reactive.util.WorldSpecificValue;
@@ -23,7 +24,7 @@ import javax.annotation.Nonnull;
 public class ReactiveMod
 {
     public static final ReactionMan REACTION_MAN = new ReactionMan();
-    //public static final DataGenerationMan DATA_GENERATION_MAN = new DataGenerationMan();
+    public static final DataGenerationMan DATA_GENERATION_MAN = new DataGenerationMan();
     public static final WorldSpecificValue WORLD_SPECIFIC_VALUE = new WorldSpecificValue();
     public static final String MODID = "reactive";
     public static final Logger LOGGER = LogManager.getLogger();
@@ -34,7 +35,7 @@ public class ReactiveMod
         MinecraftForge.EVENT_BUS.register(this);
         MinecraftForge.EVENT_BUS.register(REACTION_MAN);
         MinecraftForge.EVENT_BUS.register(WORLD_SPECIFIC_VALUE);
-        //MinecraftForge.EVENT_BUS.register(DATA_GENERATION_MAN);
+        MinecraftForge.EVENT_BUS.register(DATA_GENERATION_MAN);
         MinecraftForge.EVENT_BUS.register(SpecialCaseMan.class);
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, ConfigMan.commonSpec);
         ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, ConfigMan.serverSpec);
--- src/main/java/com/hyperlynx/reactive/datagen/DataGenerationMan.java
+++ src/main/java/com/hyperlynx/reactive/datagen/DataGenerationMan.java
@@ -1,23 +1,33 @@
-//package com.hyperlynx.reactive.datagen;
-//
-//import com.hyperlynx.reactive.ReactiveMod;
-//import net.minecraftforge.data.event.GatherDataEvent;
-//import net.minecraftforge.eventbus.api.SubscribeEvent;
-//import net.minecraftforge.fml.common.Mod;
-//
-///*
-//Looks like it's finally time for DataGeneratorMan to make an appearance!
-//Manages Forge data generation by listening for GatherDataEvents.
-//
-//Currently, it can generate:
-//- Reaction advancements
-// */
-//@Mod.EventBusSubscriber(modid= ReactiveMod.MODID, bus=Mod.EventBusSubscriber.Bus.MOD)
-//public class DataGenerationMan {
-//    @SubscribeEvent
-//    public static void gatherData (GatherDataEvent event){
-//        event.getGenerator().addProvider(
-//                event.includeServer(),
-//                new ReactiveAdvancementProvider(event.getGenerator(), event.getExistingFileHelper()));
-//    }
-//}
+package com.hyperlynx.reactive.datagen;
+
+import com.hyperlynx.reactive.ReactiveMod;
+import net.minecraft.data.DataProvider;
+import net.minecraftforge.common.data.ForgeAdvancementProvider;
+import net.minecraftforge.data.event.GatherDataEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.List;
+
+/*
+Looks like it's finally time for DataGeneratorMan to make an appearance!
+Manages Forge data generation by listening for GatherDataEvents.
+
+Currently, it can generate:
+- Reaction advancements
+ */
+@Mod.EventBusSubscriber(modid= ReactiveMod.MODID, bus=Mod.EventBusSubscriber.Bus.MOD)
+public class DataGenerationMan {
+    @SubscribeEvent
+    public static void gatherData (GatherDataEvent event){
+        event.getGenerator().addProvider(
+                event.includeServer(),
+                (DataProvider.Factory<ForgeAdvancementProvider>) output -> new ForgeAdvancementProvider(
+                        output,
+                        event.getLookupProvider(),
+                        event.getExistingFileHelper(),
+                        List.of(new ReactiveAdvancementGenerator())
+                )
+        );
+    }
+}
