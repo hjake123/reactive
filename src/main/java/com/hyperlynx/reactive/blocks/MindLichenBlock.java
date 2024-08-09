7bae588f26027e8bb687e8a946fd8a5328849d85
hjake123 <hjake123@gmail.com>
2024-08-09 12:34:48 -0500

    Mind Lichen rework: first part. Improved how it spreads.

--- src/main/java/com/hyperlynx/reactive/blocks/MindLichenBlock.java
+++ src/main/java/com/hyperlynx/reactive/blocks/MindLichenBlock.java
@@ -4,6 +4,8 @@ import com.hyperlynx.reactive.items.CrystalIronItem;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.ExperienceOrb;
@@ -14,46 +16,34 @@ import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import org.jetbrains.annotations.NotNull;
 
 public class MindLichenBlock extends GlowLichenBlock {
-    public static final BooleanProperty EMPOWERED = BooleanProperty.create("empowered");
-
     public MindLichenBlock(Properties props) {
         super(props);
-        registerDefaultState(this.defaultBlockState().setValue(EMPOWERED, false));
-    }
-
-    @Override
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(EMPOWERED);
-        super.createBlockStateDefinition(builder);
     }
 
     public void entityInside(BlockState state, Level level, BlockPos pos, Entity entity) {
-        if(state.getValue(EMPOWERED)){
-            return;
-        }
-        if (entity instanceof Player player && !player.isCreative() && player.isCrouching()) {
-            if(CrystalIronItem.effectNotBlocked(player, 1) && player.totalExperience > 10){
-                player.giveExperiencePoints(-10);
-                ExperienceOrb xp = new ExperienceOrb(level, player.getX(), player.getY() + 0.8, player.getZ(), 10);
-                double throw_power = 1.3;
-                xp.setDeltaMovement((level.getRandom().nextDouble() - 0.5) * throw_power, (level.getRandom().nextDouble() - 0.5) * throw_power, (level.getRandom().nextDouble() - 0.5) * throw_power);
-                level.addFreshEntity(xp);
+        if (entity instanceof Player player && !player.isCreative() && level.random.nextFloat() < 0.03F) {
+            if(player.totalExperience > 0){
+                for(int i = 0; i < level.random.nextIntBetweenInclusive(1, 3); i++){
+                    var to_pos = this.getSpreader().spreadFromRandomFaceTowardRandomDirection(state, level, pos, level.random);
+                    if(to_pos.isPresent()){
+                        player.giveExperiencePoints(-1);
+                        level.playSound(null, pos, SoundEvents.EXPERIENCE_ORB_PICKUP, SoundSource.BLOCKS, 0.02F, 0.7F+level.random.nextFloat()*0.1F);
+                    }
+                }
             }
             return;
         }
-        if (entity instanceof ExperienceOrb orb && level instanceof ServerLevel server){
-            for(int i = 0; i < level.random.nextIntBetweenInclusive(2, 5); i++){
-                performBonemeal(server, server.random, pos, state);
-            }
-            orb.value = orb.value - 1;
+        if (entity instanceof ExperienceOrb orb){
+            this.getSpreader().spreadFromRandomFaceTowardRandomDirection(state, level, pos, level.random);
+            orb.value -= 1;
             if(orb.value == 0){
                 orb.kill();
             }
-            level.setBlock(pos, state.setValue(EMPOWERED, true), Block.UPDATE_CLIENTS);
         }
     }
 }
