9fe7b7e7a0809e2c2a9884e30cbe77a8cccd95b6
hjake123 <hjake123@gmail.com>
2024-10-15 13:49:50 -0500

    Make Motion Salt Blocks not try to cause the air to fall.

--- src/main/java/com/hyperlynx/reactive/util/BlockMoveChecker.java
+++ src/main/java/com/hyperlynx/reactive/util/BlockMoveChecker.java
@@ -5,6 +5,7 @@ import com.hyperlynx.reactive.alchemy.AlchemyTags;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 
 import java.io.ObjectInputFilter;
@@ -16,7 +17,7 @@ public class BlockMoveChecker {
         Block candidate_to_break = state.getBlock();
         if(level.getBlockEntity(pos) != null)
             return false;
-        if(level.getBlockState(pos).is(AlchemyTags.notRelocatable))
+        if(level.getBlockState(pos).is(AlchemyTags.notRelocatable) || state.isAir())
             return false;
         return !(candidate_to_break.defaultDestroyTime() < 0) && !(candidate_to_break.defaultDestroyTime() > ConfigMan.COMMON.maxMoveBlockBreakTime.get());
     }
@@ -28,7 +29,7 @@ public class BlockMoveChecker {
     }
 
     public static boolean canMakeBlockFall(Level level, BlockPos pos, BlockState state) {
-        if(level.getBlockState(pos).is(AlchemyTags.doNotBlockFall))
+        if(state.is(AlchemyTags.doNotBlockFall))
             return false;
         return canMoveOrDisplaceBlock(level, pos, state);
     }
