b2c54aff03159348087ba077a4a91204be034b0a
hjake123 <hjake123@gmail.com>
2024-12-08 13:19:10 -0600

    Center the text

--- src/main/java/dev/hyperlynx/reactive/client/gui/LitmusScreen.java
+++ src/main/java/dev/hyperlynx/reactive/client/gui/LitmusScreen.java
@@ -1,17 +1,16 @@
 package dev.hyperlynx.reactive.client.gui;
 
-import dev.hyperlynx.reactive.components.LitmusMeasurement;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Font;
-import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.StringWidget;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 
 import java.util.List;
+import java.util.function.Supplier;
 
 public class LitmusScreen extends Screen {
     List<Component> measurement;
+
     public LitmusScreen(List<Component> measurement) {
         super(Component.translatable("item.reactive.litmus_paper"));
         this.measurement = measurement;
@@ -28,7 +27,8 @@ public class LitmusScreen extends Screen {
         int y = 0;
         for(Component component : measurement){
             StringWidget line = new StringWidget(component, Minecraft.getInstance().font);
-            line.setPosition(this.width / 2 - 100, this.height / 3 + y);
+            int line_width = line.getWidth();
+            line.setPosition(this.width / 2 - line_width / 2, this.height / 4 + y);
             line.alignCenter();
             y += 10;
             this.addRenderableWidget(line);
