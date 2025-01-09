ee508cce53124332f46d06f7940b341dd3d6f93a
hyperlynx <hjake123@gmail.com>
2025-01-08 18:41:25 -0600

    Make it less likely for Ferrous Crystals to cause ConcurrentModificationExceptions, maybe fixing issue #93

--- src/main/java/dev/hyperlynx/reactive/items/CrystalIronItem.java
+++ src/main/java/dev/hyperlynx/reactive/items/CrystalIronItem.java
@@ -2,6 +2,8 @@ package dev.hyperlynx.reactive.items;
 
 import dev.hyperlynx.reactive.Registration;
 import dev.hyperlynx.reactive.util.WorldSpecificValue;
+import net.minecraft.core.Holder;
+import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
@@ -11,6 +13,9 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 // Absorbs negative Crucible effects and Potion effects.
 public class CrystalIronItem extends Item {
@@ -47,30 +52,24 @@ public class CrystalIronItem extends Item {
 
     @Override
     public void inventoryTick(@NotNull ItemStack stack, @NotNull Level level, @NotNull Entity entity, int tick, boolean unknown) {
-        if(entity instanceof LivingEntity holder && !level.isClientSide){
-            if(holder.getActiveEffects().isEmpty()){
+        if(entity instanceof LivingEntity bearer && !level.isClientSide){
+            if(bearer.getActiveEffects().isEmpty()){
                 return;
             }
-            for(MobEffectInstance mei : holder.getActiveEffects()) {
-                if(mei.getEffect().equals(MobEffects.WITHER)){
-                    holder.removeEffect(mei.getEffect());
-                    getHurt(stack, holder);
-                }else if(mei.getEffect().equals(MobEffects.POISON)){
-                    holder.removeEffect(mei.getEffect());
-                    getHurt(stack, holder);
-                }else if(mei.getEffect().equals(MobEffects.HUNGER) && WorldSpecificValue.getBool("stone_break_hunger", 0.7F)){
-                    holder.removeEffect(mei.getEffect());
-                    getHurt(stack, holder);
-                }else if(mei.getEffect().equals(MobEffects.BAD_OMEN)){
-                    holder.removeEffect(mei.getEffect());
-                    getHurt(stack, holder);
-                }else if(mei.getEffect().equals(MobEffects.MOVEMENT_SLOWDOWN) && WorldSpecificValue.getBool("stone_break_slow", 0.3F)){
-                    holder.removeEffect(mei.getEffect());
-                    getHurt(stack, holder);
-                }else if(mei.getEffect().equals(MobEffects.WEAKNESS) && WorldSpecificValue.getBool("stone_break_weakness", 0.5F)){
-                    holder.removeEffect(mei.getEffect());
-                    getHurt(stack, holder);
-                }
+            List<MobEffect> effects_to_remove = new ArrayList<>(List.of(MobEffects.WITHER, MobEffects.POISON));
+
+            if(WorldSpecificValue.getBool("stone_break_hunger", 0.7F))
+                effects_to_remove.add(MobEffects.HUNGER);
+
+            if(WorldSpecificValue.getBool("stone_break_slow", 0.3F))
+                effects_to_remove.add(MobEffects.MOVEMENT_SLOWDOWN);
+
+            if(WorldSpecificValue.getBool("stone_break_weakness", 0.5F))
+                effects_to_remove.add(MobEffects.WEAKNESS);
+
+            for(MobEffect effect : effects_to_remove){
+                if(bearer.removeEffect(effect))
+                    getHurt(stack, bearer);
             }
         }
     }
