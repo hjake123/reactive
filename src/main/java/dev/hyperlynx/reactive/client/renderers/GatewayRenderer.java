05c95b8d1c36273201684f80148a81f3f06357e5
hyperlynx <hjake123@gmail.com>
2025-01-15 17:27:43 -0600

    Fluctuating entire cuboid

--- src/main/java/dev/hyperlynx/reactive/client/renderers/GatewayRenderer.java
+++ src/main/java/dev/hyperlynx/reactive/client/renderers/GatewayRenderer.java
@@ -7,7 +7,6 @@ import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
-import net.minecraft.core.Direction;
 import org.jetbrains.annotations.NotNull;
 import org.joml.Matrix4f;
 
@@ -21,37 +20,38 @@ public class GatewayRenderer<T extends GatewayBlockEntity> implements BlockEntit
 
     public void render(@NotNull T gateway, float partialTick, PoseStack poseStack, MultiBufferSource bufferSource, int packedLight, int packedOverlay) {
         Matrix4f matrix4f = poseStack.last().pose();
-        this.renderCube(gateway, matrix4f, bufferSource.getBuffer(RenderType.END_GATEWAY), partialTick);
+        this.renderVolume(gateway, matrix4f, bufferSource.getBuffer(RenderType.END_GATEWAY), partialTick);
     }
 
-    private void renderCube(T gateway, Matrix4f pose, VertexConsumer consumer, float partialTick) {
+    private void renderVolume(T gateway, Matrix4f pose, VertexConsumer consumer, float partialTick) {
         double time = gateway.totalTick(partialTick);
-        float distortion_1 = (float) (Math.sin(time / 50) * 0.03 + 0.95);
-        float distortion_2 = (float) (Math.sin(time / 55) * 0.03 + 0.95);
-        float distortion_3 = (float) (Math.sin(time / 48) * 0.032 + 0.95);
-        float distortion_4 = (float) (Math.sin(time / 52) * 0.032 + 0.95);
-        float distortion_5 = (float) (Math.sin((time / 50) + 0.5) * 0.03 + 0.95);
-        float distortion_6 = (float) (Math.sin((time / 55) + 0.5) * 0.03 + 0.95);
-        float distortion_7 = (float) (Math.sin((time / 48) + 0.5) * 0.032 + 0.95);
-        float distortion_8 = (float) (Math.sin((time / 52) + 0.5) * 0.032 + 0.95);
+        float amplitude = 0.1F;
+        float distortion_1 = (float) (Math.sin(time / 50) * amplitude + 0.95);
+        float distortion_2 = (float) (Math.sin(time / 55) * amplitude + 0.95);
+        float distortion_3 = (float) (Math.sin(time / 48) * amplitude + 0.95);
+        float distortion_4 = (float) (Math.sin(time / 52) * amplitude + 0.95);
+        float distortion_5 = (float) (Math.sin((time / 50) + 0.5) * amplitude + 0.95);
+        float distortion_6 = (float) (Math.sin((time / 55) + 0.5) * amplitude + 0.95);
+        float distortion_7 = (float) (Math.sin((time / 48) + 0.5) * amplitude + 0.95);
+        float distortion_8 = (float) (Math.sin((time / 52) + 0.5) * amplitude + 0.95);
 
         // SOUTH, NORTH
         this.renderFace(pose, consumer, 0.0F, 1.0F, 0.0F, 1.0F, 1.0F , 1.0F, 1.0F, 1.0F,
                 distortion_3, distortion_1, distortion_4, distortion_2);
         this.renderFace(pose, consumer, 0.0F, 1.0F, 1.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F,
-                1.0F, 1.0F, 1.0F, 1.0F);
+                distortion_7, distortion_5, distortion_6, distortion_8);
 
         // EAST, WEST
         this.renderFace(pose, consumer, 1.0F, 1.0F, 1.0F, 0.0F, 0.0F, 1.0F, 1.0F, 0.0F,
-                1.0F, 1.0F, distortion_2, distortion_4);
+                distortion_6, distortion_8, distortion_2, distortion_4);
         this.renderFace(pose, consumer, 0.0F, 0.0F, 0.0F, 1.0F, 0.0F, 1.0F, 1.0F, 0.0F,
-                1.0F, 1.0F, distortion_3, distortion_1);
+                distortion_5, distortion_7, distortion_3, distortion_1);
 
         // DOWN, UP
         this.renderFace(pose, consumer, 0.0F, 1.0F, 0.0F, 0.0F, 0.0F, 0.0F, 1.0F, 1.0F,
-                distortion_1, 1.0F, distortion_2, 1.0F);
+                distortion_1, distortion_7, distortion_2, distortion_6);
         this.renderFace(pose, consumer, 0.0F, 1.0F, 1.0F, 1.0F, 1.0F, 1.0F, 0.0F, 0.0F,
-                1.0F, distortion_3, 1.0F, distortion_4);
+                distortion_5, distortion_3, distortion_8, distortion_4);
     }
 
     private void renderFace(Matrix4f pose, VertexConsumer consumer, float x0, float x1, float y0, float y1, float z0, float z1, float z2, float z3, float ul_distort, float dl_distort, float ur_distort, float dr_distort) {
