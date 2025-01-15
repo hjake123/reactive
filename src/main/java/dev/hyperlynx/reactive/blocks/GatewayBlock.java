3c464844a18b1307137d8dcbd5b16c15bb72b47c
hyperlynx <hjake123@gmail.com>
2025-01-15 17:32:56 -0600

    Hide the bounding box inside the rift

--- src/main/java/dev/hyperlynx/reactive/blocks/GatewayBlock.java
+++ src/main/java/dev/hyperlynx/reactive/blocks/GatewayBlock.java
@@ -5,6 +5,7 @@ import dev.hyperlynx.reactive.be.GatewayBlockEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
@@ -15,10 +16,15 @@ import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.portal.DimensionTransition;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class GatewayBlock extends Block implements Portal, EntityBlock {
+    private final VoxelShape SHAPE = Block.box(6, 6, 6, 10, 10, 10);
+
     public GatewayBlock(Properties properties) {
         super(properties);
     }
@@ -51,5 +57,10 @@ public class GatewayBlock extends Block implements Portal, EntityBlock {
     @Override
     public @NotNull RenderShape getRenderShape(@NotNull BlockState state) {
         return RenderShape.ENTITYBLOCK_ANIMATED;
+    }
+
+    @Override
+    protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return SHAPE;
     }
 }
