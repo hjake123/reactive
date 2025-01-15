475a65d91e5a7c442bb773aa1cf8ccce4787cb4b
hyperlynx <hjake123@gmail.com>
2025-01-15 16:45:05 -0600

    Succeed at distorting one vertex

--- src/main/java/dev/hyperlynx/reactive/client/renderers/GatewayRenderer.java
+++ src/main/java/dev/hyperlynx/reactive/client/renderers/GatewayRenderer.java
@@ -29,22 +29,34 @@ public class GatewayRenderer<T extends GatewayBlockEntity> implements BlockEntit
         float distortion = (float) (Math.sin(time / 50) * 0.03 + 0.04);
 
         // SOUTH, NORTH
-        this.renderFace(pose, consumer, 0.0F, 1.0F, 0.0F, 1.0F, 1.0F , 1.0F, 1.0F, 1.0F);
-        this.renderFace(pose, consumer, 0.0F, 1.0F, 1.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F);
+        this.renderFace(pose, consumer, 0.0F, 1.0F, 0.0F, 1.0F, 1.0F , 1.0F, 1.0F, 1.0F,
+                1.0F, 0.9F, 1.0F, 1.0F);
+        this.renderFace(pose, consumer, 0.0F, 1.0F, 1.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F,
+                1.0F, 1.0F, 1.0F, 1.0F);
 
         // EAST, WEST
-        this.renderFace(pose, consumer, 1.0F, 1.0F, 1.0F, 0.0F, 0.0F, 1.0F, 1.0F, 0.0F);
-        this.renderFace(pose, consumer, 0.0F, 0.0F, 0.0F, 1.0F, 0.0F, 1.0F, 1.0F, 0.0F);
+        this.renderFace(pose, consumer, 1.0F, 1.0F, 1.0F, 0.0F, 0.0F, 1.0F, 1.0F, 0.0F,
+                1.0F, 1.0F, 1.0F, 1.0F);
+        this.renderFace(pose, consumer, 0.0F, 0.0F, 0.0F, 1.0F, 0.0F, 1.0F, 1.0F, 0.0F,
+                1.0F, 1.0F, 1.0F, 0.9F);
 
         // DOWN, UP
-        this.renderFace(pose, consumer, 0.0F, 1.0F, 0.0F, 0.0F, 0.0F, 0.0F, 1.0F, 1.0F);
-        this.renderFace(pose, consumer, 0.0F, 1.0F, 1.0F, 1.0F, 1.0F, 1.0F, 0.0F, 0.0F);
+        this.renderFace(pose, consumer, 0.0F, 1.0F, 0.0F, 0.0F, 0.0F, 0.0F, 1.0F, 1.0F,
+                0.9F, 1.0F, 1.0F, 1.0F);
+        this.renderFace(pose, consumer, 0.0F, 1.0F, 1.0F, 1.0F, 1.0F, 1.0F, 0.0F, 0.0F,
+                1.0F, 1.0F, 1.0F, 1.0F);
     }
 
-    private void renderFace(Matrix4f pose, VertexConsumer consumer, float x0, float x1, float y0, float y1, float z0, float z1, float z2, float z3) {
-        consumer.addVertex(pose, x0, y0, z0);
-        consumer.addVertex(pose, x1, y0, z1);
-        consumer.addVertex(pose, x1, y1, z2);
-        consumer.addVertex(pose, x0, y1, z3);
+    private void renderFace(Matrix4f pose, VertexConsumer consumer, float x0, float x1, float y0, float y1, float z0, float z1, float z2, float z3, float ul_distort, float dl_distort, float ur_distort, float dr_distort) {
+        consumer.addVertex(pose, adjust(x0, dl_distort), adjust(y0,  dl_distort), adjust(z0, dl_distort));
+        consumer.addVertex(pose, adjust(x1,  dr_distort), adjust(y0,  dr_distort), adjust(z1, dr_distort));
+        consumer.addVertex(pose, adjust(x1,  ur_distort), adjust(y1,  ur_distort), adjust(z2, ur_distort));
+        consumer.addVertex(pose, adjust(x0,  ul_distort), adjust(y1,  ul_distort), adjust(z3, ul_distort));
+    }
+
+    private float adjust(float base, float distort_factor){
+        float core_dist = base - 0.5F;
+        float result_dist = core_dist * distort_factor;
+        return result_dist + 0.5F;
     }
 }
