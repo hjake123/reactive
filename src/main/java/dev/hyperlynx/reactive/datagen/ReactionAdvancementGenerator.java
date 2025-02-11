22735c4aee45cda99c5ce3cc8fac93b5ece7b617
hyperlynx <hjake123@gmail.com>
2025-02-11 12:25:38 -0600

    Replace the reaction criteria builder with a new generic singleton criterion for reactions that takes an alias as a condition

--- src/main/java/dev/hyperlynx/reactive/datagen/ReactionAdvancementGenerator.java
+++ src/main/java/dev/hyperlynx/reactive/datagen/ReactionAdvancementGenerator.java
@@ -1,7 +1,7 @@
 package dev.hyperlynx.reactive.datagen;
 
 import dev.hyperlynx.reactive.ReactiveMod;
-import dev.hyperlynx.reactive.alchemy.rxn.ReactionMan;
+import dev.hyperlynx.reactive.Registration;
 import net.minecraft.advancements.Advancement;
 import net.minecraft.advancements.AdvancementHolder;
 import net.minecraft.advancements.AdvancementRequirements;
@@ -10,36 +10,34 @@ import net.minecraft.core.HolderLookup;
 import net.neoforged.neoforge.common.data.AdvancementProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 
 /*
 Automatically generates advancements for each reaction with a defined FlagCriterion!
  */
-public class ReactiveAdvancementGenerator implements AdvancementProvider.AdvancementGenerator {
+public class ReactionAdvancementGenerator implements AdvancementProvider.AdvancementGenerator {
+    private static final List<String> aliases = new ArrayList<>();
+
+    public static void add(String alias) {
+        aliases.add(alias);
+    }
+
     @Override
     public void generate(HolderLookup.Provider registries, Consumer<AdvancementHolder> consumer, ExistingFileHelper existingFileHelper) {
-        // Generate and save the reaction advancements based on the list given to ReactionMan.CRITERIA_BUILDER
+        // Generate and save the reaction advancements based on the list given to this class;
         String REACTION_ADVANCEMENT_PREFIX = ":reactions/";
-        List<String> aliases = ReactionMan.CRITERIA_BUILDER.getAliases();
         for(String alias : aliases){
-            if(ReactionMan.CRITERIA_BUILDER.get(alias) == null) {
-                System.err.println("Reaction " + alias + " doesn't have a criterion, so no advancement will be generated.");
-                continue;
-            }
             Advancement.Builder builder = Advancement.Builder.advancement();
-            builder.addCriterion("criterion", ReactionMan.CRITERIA_BUILDER.get(alias).instance());
+            builder.addCriterion("criterion", Registration.REACTION_TRIGGER.get().instance(alias));
             builder.requirements(AdvancementRequirements.Strategy.AND);
             builder.rewards(AdvancementRewards.EMPTY);
             builder.save(consumer, ReactiveMod.MODID + REACTION_ADVANCEMENT_PREFIX + alias);
 
             // Generate the "perfection" advancements for getting the requirements exactly right
-            if(ReactionMan.CRITERIA_BUILDER.get(alias+"_perfect") == null) {
-                System.err.println("Reaction " + alias + " doesn't have a perfection criterion, so no perfection advancement will be generated.");
-                continue;
-            }
             Advancement.Builder perfect_builder = Advancement.Builder.advancement();
-            perfect_builder.addCriterion("criterion", ReactionMan.CRITERIA_BUILDER.get(alias+"_perfect").instance());
+            perfect_builder.addCriterion("criterion", Registration.PERFECT_REACTION_TRIGGER.get().instance(alias));
             perfect_builder.requirements(AdvancementRequirements.Strategy.AND);
             perfect_builder.rewards(AdvancementRewards.EMPTY);
             perfect_builder.save(consumer, ReactiveMod.MODID +REACTION_ADVANCEMENT_PREFIX + alias +"_perfect");
