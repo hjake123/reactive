2cefec4f4c798dfefb1f558a6adfcff3b12993e7
hyperlynx <hjake123@gmail.com>
2025-02-18 17:37:48 -0600

    It'd help if you actually invoked any of the packet stuff...

--- src/main/java/dev/hyperlynx/reactive/integration/patchouli/ReactionComponentProcessor.java
+++ src/main/java/dev/hyperlynx/reactive/integration/patchouli/ReactionComponentProcessor.java
@@ -5,6 +5,7 @@ import dev.hyperlynx.reactive.alchemy.Power;
 import dev.hyperlynx.reactive.alchemy.rxn.CatalystEffectReaction;
 import dev.hyperlynx.reactive.alchemy.rxn.CurseAssimilationReaction;
 import dev.hyperlynx.reactive.alchemy.rxn.Reaction;
+import dev.hyperlynx.reactive.net.rxn.ReactionPageFetcher;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.level.Level;
 import vazkii.patchouli.api.IComponentProcessor;
@@ -21,45 +22,14 @@ public class ReactionComponentProcessor implements IComponentProcessor {
     @Override
     public IVariable process(Level level, String key) {
         if(key.equals("formula")){
-            for(Reaction reaction : ReactiveMod.REACTION_MAN.getReactions()){
-                if(reaction.getAlias().equals(reaction_alias)){
-                    StringBuilder formula = new StringBuilder();
-                    formula.append(Component.translatable("docs.reactive.powers_label").getString());
-                    for(Power power : reaction.getReagents().keySet()){
-                        formula.append(power.getName()).append("$(br)");
-                    }
-                    if(reaction instanceof CurseAssimilationReaction){
-                        formula.append(Component.translatable("docs.reactive.curse_label").getString());
-                    }
-                    if(reaction.getReagents().keySet().isEmpty()){
-                        formula.append(Component.translatable("docs.reactive.any_label").getString());
-                    }
-                    formula.append(Component.translatable("docs.reactive.stimulus_label").getString());
-                    switch(reaction.getStimulus()){
-                        case NONE -> formula.append(Component.translatable("text.reactive.none").getString());
-                        case GOLD_SYMBOL -> formula.append(Component.translatable("block.reactive.gold_symbol").getString());
-                        case ELECTRIC -> formula.append(Component.translatable("text.reactive.electric_charge").getString());
-                        case SACRIFICE -> formula.append(Component.translatable("text.reactive.sacrificial").getString());
-                        case END_CRYSTAL -> formula.append(Component.translatable("item.minecraft.end_crystal").getString());
-                        case NO_ELECTRIC -> formula.append(Component.translatable("text.reactive.lack_of").getString()).append(Component.translatable("text.reactive.electric_charge").getString());
-                        case NO_END_CRYSTAL -> formula.append(Component.translatable("text.reactive.lack_of").getString()).append(Component.translatable("item.minecraft.end_crystal").getString());
-                    }
-                    if(reaction instanceof CatalystEffectReaction catre){
-                        formula.append(Component.translatable("docs.reactive.catalyst_label").getString());
-                        formula.append(catre.getCatalyst().getDescription().getString());
-                    }
-                    return IVariable.wrap(formula.toString());
-                }
+            try {
+                return IVariable.wrap(ReactionPageFetcher.requestFormulaFor(reaction_alias));
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
-            return IVariable.empty();
         }
         if(key.equals("lock")){
-            for(Reaction reaction : ReactiveMod.REACTION_MAN.getReactions()) {
-                if (reaction.getAlias().equals(reaction_alias)) {
-                    return IVariable.wrap("reactive:reactions/" + reaction_alias + "_perfect");
-                }
-            }
-            return IVariable.empty();
+            return IVariable.wrap("reactive:reactions/" + reaction_alias + "_perfect");
         }
         return IVariable.empty();
     }
