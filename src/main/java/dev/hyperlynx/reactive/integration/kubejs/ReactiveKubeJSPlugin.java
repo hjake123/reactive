82fc556e04015427f4471efefb1a1a06ef9297f7
hyperlynx <hjake123@gmail.com>
2025-05-12 17:44:03 -0500

    Bump KubeJS to use its new maven

--- src/main/java/dev/hyperlynx/reactive/integration/kubejs/ReactiveKubeJSPlugin.java
+++ src/main/java/dev/hyperlynx/reactive/integration/kubejs/ReactiveKubeJSPlugin.java
@@ -43,7 +43,7 @@ public class ReactiveKubeJSPlugin implements KubeJSPlugin {
         registry.addDefault(Powers.POWER_REGISTRY_KEY, KubePowerBuilder.class, KubePowerBuilder::new);
         registry.addDefault(Registries.TRIGGER_TYPE, FlagTriggerBuilder.class, FlagTriggerBuilder::new);
         registry.of(Registries.ITEM, reg -> {
-            reg.add("reactive:power_bottle", CustomPowerBottleItem.Builder.class, CustomPowerBottleItem.Builder::new);
+            reg.add(ReactiveMod.location("power_bottle"), CustomPowerBottleItem.Builder.class, CustomPowerBottleItem.Builder::new);
         });
     }
 
