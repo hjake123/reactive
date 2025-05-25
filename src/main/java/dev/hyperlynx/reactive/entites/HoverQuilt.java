4c245ec37d8f95038e9168621908b480b4a41ae5
hyperlynx <hjake123@gmail.com>
2025-05-25 14:25:51 -0500

    Add basic movement mechanics. Currently causes a desync when dismounted

--- src/main/java/dev/hyperlynx/reactive/entites/HoverQuilt.java
+++ src/main/java/dev/hyperlynx/reactive/entites/HoverQuilt.java
@@ -1,18 +1,21 @@
 package dev.hyperlynx.reactive.entites;
 
 import dev.hyperlynx.reactive.Registration;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.ServerboundPacketListener;
+import net.minecraft.network.protocol.game.ServerboundMoveVehiclePacket;
 import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.AnimationState;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.vehicle.VehicleEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class HoverQuilt extends VehicleEntity {
     public final AnimationState hovering = new AnimationState();
@@ -51,6 +54,11 @@ public class HoverQuilt extends VehicleEntity {
     }
 
     @Override
+    protected @NotNull Vec3 getPassengerAttachmentPoint(Entity entity, EntityDimensions dimensions, float partialTick) {
+        return getDefaultPassengerAttachmentPoint(this, entity, dimensions.attachments()).add(0, 0.05, 0);
+    }
+
+    @Override
     protected void defineSynchedData(SynchedEntityData.Builder builder) {
         super.defineSynchedData(builder);
     }
@@ -66,11 +74,38 @@ public class HoverQuilt extends VehicleEntity {
     }
 
     @Override
+    public @Nullable LivingEntity getControllingPassenger() {
+        if(this.getFirstPassenger() instanceof LivingEntity driver) {
+            return driver;
+        }
+        return null;
+    }
+
+    private static final double MAX_SPEED = 0.25;
+
+    @Override
     public void tick() {
         super.tick();
         if(this.level().isClientSide()) {
             animation_timer++;
+            if(this.isVehicle() && this.isControlledByLocalInstance()) {
+                LivingEntity riding_entity = this.getControllingPassenger();
+                if(riding_entity instanceof LocalPlayer rider) {
+                    double vertical_speed = this.getDeltaMovement().y;
+                    if(rider.input.up || rider.input.jumping) {
+                        vertical_speed = Math.min(MAX_SPEED, vertical_speed + 0.03);
+                    }
+                    if(rider.input.down) {
+                        vertical_speed = Math.max(-MAX_SPEED, vertical_speed - 0.03);
+                    }
+                    this.setDeltaMovement(0, vertical_speed, 0);
+                    this.move(MoverType.PLAYER, this.getDeltaMovement());
+                }
+            } else if(!this.isVehicle()) {
+                this.setDeltaMovement(0, 0, 0);
+            }
         }
+
     }
 
     @Override
