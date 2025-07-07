8eedeac6fa597a527c092b247c93ec8154a9c0a6
hyperlynx <hjake123@gmail.com>
2025-07-07 00:03:06 -0500

    Add basic rename button, making the desk technically kind of functional!

--- src/main/java/dev/hyperlynx/reactive/client/gui/DeskScreen.java
+++ src/main/java/dev/hyperlynx/reactive/client/gui/DeskScreen.java
@@ -1,14 +1,40 @@
 package dev.hyperlynx.reactive.client.gui;
 
+import dev.hyperlynx.reactive.alchemy.material.ClientMaterialMan;
 import dev.hyperlynx.reactive.menu.DeskMenu;
+import dev.hyperlynx.reactive.registration.ReactiveComponentTypes;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.item.ItemStack;
 
 public class DeskScreen extends AbstractContainerScreen<DeskMenu> {
+    Button rename_button = new Button.Builder(Component.translatable("text.reactive.rename"),
+            button -> {
+                if(this.getMenu().getSlot(0).hasItem()) {
+                    ItemStack stack = this.getMenu().getSlot(0).getItem();
+                    ResourceLocation location = stack.get(ReactiveComponentTypes.MATERIAL_ID.get());
+                    if(location != null) {
+                        ScreenOpener.materialRename(location);
+                    }
+                }
+            }).build();
+
+
     public DeskScreen(DeskMenu menu, Inventory playerInventory, Component title) {
         super(menu, playerInventory, title);
+    }
+
+    @Override
+    protected void init() {
+        super.init();
+        rename_button.setPosition(30, 10);
+        addRenderableWidget(rename_button);
     }
 
     @Override
