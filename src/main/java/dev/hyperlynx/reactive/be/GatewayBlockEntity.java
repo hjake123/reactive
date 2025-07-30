c2dd4b5a1a07986c00f5cc68166317be5fe64f3c
hyperlynx <hjake123@gmail.com>
2025-07-30 14:23:34 -0500

    Tweaked a few things and removed vestigial code

--- src/main/java/dev/hyperlynx/reactive/be/GatewayBlockEntity.java
+++ src/main/java/dev/hyperlynx/reactive/be/GatewayBlockEntity.java
@@ -9,7 +9,6 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtUtils;
 import net.minecraft.nbt.StringTag;
-import net.minecraft.nbt.Tag;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.Level;
--- src/main/java/dev/hyperlynx/reactive/items/PhantomQuiltItem.java
+++ src/main/java/dev/hyperlynx/reactive/items/PhantomQuiltItem.java
@@ -5,27 +5,16 @@ import dev.hyperlynx.reactive.registration.ReactiveEntityTypes;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
-import org.jetbrains.annotations.NotNull;
 
 public class PhantomQuiltItem extends Item {
     public PhantomQuiltItem(Properties pProperties) {
         super(pProperties);
-    }
-
-    private static final int ACTIVATE_HEIGHT = 20;
-
-    @Override
-    public void inventoryTick(@NotNull ItemStack stack, @NotNull Level level, @NotNull Entity entity, int slot, boolean selected){
-        if (entity.fallDistance > ACTIVATE_HEIGHT) {
-            // TODO: Maybe summon the entity below you?
-        }
     }
 
     @Override
